154721558d1947cae74316d6624023d01a98d7b2
avangardewashere <avelpanaligan@gmail.com>
2025-01-17 17:45:26 +0800

    update/document-page-layout

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,36 @@
-"use client"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
   return (
-    <div>
-      <span>This is a protected Page</span>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        className="dark:hidden"
+        height={40}
+        src="/empty-image.png"
+        width={40}
+        alt="solution-logo"
+      />
+      <Image
+        className="hidden dark:block"
+        height={40}
+        src="/empty-image.png"
+        style={{ filter: "invert(100%) brightness(150%)" }}
+        width={40}
+        alt="solution-logo"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Solution
+      </h2>
+      <Button className="bg-zinc-300 rounded-xl" variant={"outline"}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
