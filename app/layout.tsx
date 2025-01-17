002d6f08821146bb8013ff44bdb20e55d2ce0ff2
avangardewashere <avelpanaligan@gmail.com>
2025-01-17 11:29:10 +0800

    add/theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,45 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Solution',
-  description: 'Connected Workspace for everyone',
-  icons:{
-    icon:[
- 
+  title: "Solution",
+  description: "Connected Workspace for everyone",
+  icons: {
+    icon: [
       {
-        media:"(prefers-color-scheme:light)",
-        url:"/solution.png",
-        href:"/solution.png"
+        media: "(prefers-color-scheme:light)",
+        url: "/solution.png",
+        href: "/solution.png",
       },
       {
-        media:"(prefers-color-scheme:dark)",
-        url:"/dark-solution.png",
-        href:"/dark-solution.png"
-      }
-    ]
-  }
-}
+        media: "(prefers-color-scheme:dark)",
+        url: "/dark-solution.png",
+        href: "/dark-solution.png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
