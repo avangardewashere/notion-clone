4db3b07dbc69da6316b21e229ccd9655d2661fb0
avangardewashere <avelpanaligan@gmail.com>
2025-01-19 23:52:31 +0800

    add/imports

--- app/(main)/_components/trashbox.tsx
+++ app/(main)/_components/trashbox.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import { useParams, useRouter } from "next/navigation"
+
 const TrashBox = () => {
+    const router = useRouter()
+    const params = useParams();
+    
   return (
     <div>
       
