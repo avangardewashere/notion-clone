ce8c780a5dba60679fa6709a208011ac96fef3bf
avangardewashere <avelpanaligan@gmail.com>
2025-01-19 23:58:33 +0800

    add/useQury

--- app/(main)/_components/trashbox.tsx
+++ app/(main)/_components/trashbox.tsx
@@ -1,11 +1,12 @@
 "use client"
 
+import { useQuery } from "convex/react";
 import { useParams, useRouter } from "next/navigation"
 
 const TrashBox = () => {
     const router = useRouter()
     const params = useParams();
-    
+    const documents = useQuery()
   return (
     <div>
       
