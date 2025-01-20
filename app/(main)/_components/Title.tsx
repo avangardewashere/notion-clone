d192b1d84a9133c575de817c1e6a964076bfbbb6
avangardewashere <avelpanaligan@gmail.com>
2025-01-20 14:58:45 +0800

    add/update

--- app/(main)/_components/Title.tsx
+++ app/(main)/_components/Title.tsx
@@ -1,17 +1,71 @@
- 'use client'
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRef, useState } from "react";
 
- interface TitleProps {
-    initialData:Doc<"documents">;
- }
-
-const Title:React.FC<TitleProps> = ({initialData}) => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface TitleProps {
+  initialData: Doc<"documents">;
 }
 
-export default Title
+const Title: React.FC<TitleProps> = ({ initialData }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const update = useMutation(api.documents.update);
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(initialData.title || "Untitled");
+
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    update({
+      id: initialData._id,
+      title: e?.target.value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      disableInput();
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input
+          className="h-7 p-2 focus-visible:ring-transparent"
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      ) : (
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={enableInput}
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData?.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Title;
