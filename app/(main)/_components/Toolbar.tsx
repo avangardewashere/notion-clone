a7481f72e8e7d49020ffa68253ed6f83ca4cf591
avangardewashere <avelpanaligan@gmail.com>
2025-01-21 03:39:49 +0800

    tbc/edge-store-setup

--- app/(main)/_components/Toolbar.tsx
+++ app/(main)/_components/Toolbar.tsx
@@ -4,6 +4,7 @@ import { IconPicker } from "@/components/icon-picker";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useCoverImage } from "@/hooks/use-cover-image";
 import { useMutation } from "convex/react";
 import { ImageIcon, Smile, X } from "lucide-react";
 import { ElementRef, useRef, useState } from "react";
@@ -20,6 +21,8 @@ const Toolbar: React.FC<ToolBarProps> = ({ initialData, preview }) => {
 
   const update = useMutation(api.documents.update);
   const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImage();
 
   const enableInput = () => {
     if (preview) return;
@@ -103,7 +106,7 @@ const Toolbar: React.FC<ToolBarProps> = ({ initialData, preview }) => {
 
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-slate-300 text-xs"
             variant={"outline"}
             size="sm"
