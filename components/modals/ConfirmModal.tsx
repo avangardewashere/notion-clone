eb41c69921e831d13c0d876b373910b97e3432f3
avangardewashere <avelpanaligan@gmail.com>
2025-01-20 08:57:18 +0800

    add/Confirmation-modal

---
+++ components/modals/ConfirmModal.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal: React.FC<ConfirmModalProps> = ({
+  children,
+  onConfirm,
+}) => {
+  const handleConfirm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>{children}</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+
+          <AlertDialogDescription>
+            This Action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+        <AlertDialogCancel onClick={(e)=>e.stopPropagation()}>
+            Cancel
+        </AlertDialogCancel>
+        <AlertDialogAction onClick={handleConfirm}>
+            Confirm
+        </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
