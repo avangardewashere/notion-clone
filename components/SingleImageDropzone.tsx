d9aca13efad17dac33686c9b3ddabacac1ab2e51
avangardewashere <avelpanaligan@gmail.com>
2025-01-22 06:24:59 +0800

    add/single-image-dropzone

--- components/SingleImageDropzone.tsx
+++ components/SingleImageDropzone.tsx
@@ -153,7 +153,7 @@ const SingleImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
             // Upload Icon
             <div className="flex flex-col items-center justify-center text-xs text-gray-400">
               <UploadCloudIcon className="mb-2 h-7 w-7" />
-              <div className="text-gray-400">drag & drop to upload</div>
+              <div className="text-gray-400">Click or drag fie to this area to upload</div>
               {/* <div className="mt-3">
                 <Button type="button" disabled={disabled}>
                   select
