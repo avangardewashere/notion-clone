daa744e14545b3f4e7ec5147b7765029a355bb29
avangardewashere <avelpanaligan@gmail.com>
2025-01-22 03:25:01 +0800

    update/cover-image-modal

--- components/modals/CoverImageModal.tsx
+++ components/modals/CoverImageModal.tsx
@@ -1,9 +1,42 @@
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { Dialog } from "@radix-ui/react-dialog";
 import { DialogContent, DialogHeader } from "../ui/dialog";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const update = useMutation(api.documents.update);
   const coverImage = useCoverImage();
+  const params = useParams();
+  const { edgestore } = useEdgeStore();
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({ file });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
