da091d3a7801360a554498030a736d62195b42a2
avangardewashere <avelpanaligan@gmail.com>
2025-01-22 16:29:44 +0800

    add/handle-upload-text-editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -4,10 +4,10 @@ import "@blocknote/core/style.css";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
-import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import { useEdgeStore } from "@/lib/edgestore";
+
 interface EditorProps {
-    onChange: (content: string) => void; 
+  onChange: (content: string) => void;
   initialContent?: string;
   editable?: boolean;
 }
@@ -17,21 +17,27 @@ const Editor: React.FC<EditorProps> = ({
   initialContent,
   editable,
 }) => {
+  const edgestore = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const res = await edgestore.edgestore.publicFiles.upload({
+      file,
+    });
+
+    return res.url;
+  };
+
   const editor: BlockNoteEditor = useCreateBlockNote({
     // editable:true,
     initialContent: initialContent
       ? (JSON.parse(initialContent) as PartialBlock[])
       : undefined,
+    uploadFile: handleUpload,
   });
-
-  const update= useMutation(api.documents.update)
-
-
-
 
   return (
     <div>
-  <BlockNoteView editor={editor} theme="light" />;
+      <BlockNoteView editor={editor} theme="light" />;
     </div>
   );
 };
