be796e68bab89c1567a2f90ce9f32a2b23a74881
avangardewashere <avelpanaligan@gmail.com>
2025-01-22 13:59:56 +0800

    install/blocknote-add-editor-component

---
+++ components/Editor.tsx
@@ -0,0 +1,16 @@
+ 'use-client'
+
+interface EditorProps {
+    onChange:()=>void;
+    initialContent:id<"documents">;
+}
+
+const Editor = () => {
+  return (
+    <div>
+      <span></span>
+    </div>
+  )
+}
+
+export default Editor
