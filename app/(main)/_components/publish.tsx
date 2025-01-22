35028917597edd5e7f43c1cca70715507858cf2d
avangardewashere <avelpanaligan@gmail.com>
2025-01-23 03:04:23 +0800

    add/publish.tsx

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,15 +1,138 @@
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
-import React from "react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useMutation } from "convex/react";
+import { Check, Copy, Globe } from "lucide-react";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
 interface PublishProps {
   initialData: Doc<"documents">;
 }
 
 const Publish = ({ initialData }: PublishProps) => {
+  const origin = useOrigin();
+  const update = useMutation(api.documents.update);
+
+  const [copied, setCopied] = useState(false);
+  const [isSubmittin, setIsSubmitting] = useState(false);
+
+  const url = `${origin}/preview/${initialData._id}`;
+
+  const onPublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: true,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Publishing...",
+      success: "Note published",
+      error: "Failed to publish note.",
+    });
+  };
+
+  const onUnpublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: false,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Unpublishing...",
+      success: "Note unpublished",
+      error: "Failed to unpublish note.",
+    });
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(url);
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
   return (
-    <div>
-      <span>Publish</span>
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size="sm" variant="ghost">
+          Publish
+          {initialData.isPublished && (
+            <Globe className="text-sky-500 w-4 h-4 ml-2" />
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
+        {initialData.isPublished ? (
+          <div className="space-y-4">
+            <div className="flex items-center gap-x-2">
+              <Globe className="text-sky-500 animatepulse h-4 w-4" />
+              <p className="text-xs font-medium text-sky-500">
+                This note is live on web
+              </p>
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  className="flex-1 text-xs px-2 border rounded-l-md h-8 bg-muted truncate "
+                  value={url}
+                  disabled
+                />
+                <Button
+                  onClick={onCopy}
+                  disabled={copied}
+                  className="h-8 rounded-l-none"
+                >
+                  {copied ? (
+                    <Check className="h-4 w-4" />
+                  ) : (
+                    <Copy className="h4 w-4" />
+                  )}
+                </Button>
+              </div>
+              <Button
+                className="w-ful text-xs"
+                disabled={isSubmittin}
+                onClick={onUnpublish}
+                size="sm"
+              >
+                Unpublish
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col items-center justiy-between">
+              <Globe className="h8 w-8 text-slate-400 mb-2" />
+              <p className="text-sm font-medium mb-2">Publish this note</p>
+              <span className="text-xs text-slate-300 fmb-4 ">
+                Share your work with others
+              </span>
+              <Button
+                disabled={isSubmittin}
+                className="w-full text-sx"
+                size="sm"
+                onClick={onPublish}
+              >
+                Publish
+              </Button>
+            </div>
+          </>
+        )}
+      </PopoverContent>
+    </Popover>
   );
 };
 
