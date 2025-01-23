21c5c596207a75515a4f546e1475fd6e4ba6a7d3
avangardewashere <avelpanaligan@gmail.com>
2025-01-23 11:36:58 +0800

    debug/main

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,8 +3,9 @@ import Navbar from "@/components/ui/Navbar";
  const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full dark:bg-[#1f1f1f]">
-      <Navbar />
-      <main className="h-full pt-40">{children}</main>
+      main marketing
+      {/* <Navbar />
+      <main className="h-full pt-40">{children}</main> */}
     </div>
   );
 };
